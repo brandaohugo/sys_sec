416f2eaabdcad39aa5309e772c9d08b265ef6278
Hugo Brandao <brandaohugo@hotmail.com>
2016-10-03 11:36:26 +0200

    adding individual block comparison and numb of arguments checking

--- 2/blockhash.py
+++ 2/blockhash.py
@@ -29,13 +29,23 @@ def hash_blocks(blocks):
 		result.append(sha256(b).hexdigest())
 	return result
 
+
+if (len(sys.argv) != 4):
+	print("Usage: pyhton blockhash.py <filename1> <filename2> <number_of_blocks>")
+	exit(1);
+
 l1 = segment_file(sys.argv[1], (int) (sys.argv[3]))
 l2 = segment_file(sys.argv[2], (int) (sys.argv[3]))
 
 h1 = hash_blocks(l1)
 h2 = hash_blocks(l2)
 
-if (h1 == h2):
-	print("The files are the same")
-else:
-	print("The files are not the same")
+matches = 0
+for i in range(0,(int) (sys.argv[3])):
+	if (h1[i] == h2[i]):
+		print("Blocks in position %d match.\n" % i)
+		matches += 1
+	else:
+		print("Blocks in postiion %d do not match.\n" % i)
+
+print("%.2f%% compatibility." % 100 * (matches / (int)(sys.argv[3])))
